293e5a793f00307b6d399db674edaaee593ceaa6
renato-17 <renato.avela.17@gmail.com>
2023-01-11 13:17:44 -0500

    <feat>: adding loader to submit button

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,9 +6,11 @@ interface ModalProp {
 
 const Modal = ({ children }: ModalProp): ReactElement => {
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-gray-light flex justify-center items-start z-[100]">
-      <div className="w-[80%] md:max-w-[700px] bg-white p-5 rounded-xl mt-10">
-        { children }
+    <div className={'fixed top-0 left-0 mt-10 w-full h-full bg-gray-light flex justify-center items-start z-[100]'}>
+      <div className={'w-[100%] md:max-w-[700px]'}>
+        <div className={'w-full bg-white rounded-xl relative'}>
+            {children}
+        </div>
       </div>
     </div>
   )
--- src/components/certificate/ImportExcel.tsx
+++ src/components/certificate/ImportExcel.tsx
@@ -12,6 +12,7 @@ const ImportExcel = ({ close, addCertificates }: ImportExcelProps): ReactElement
   const certificatesService = new CertificatesService()
   const [file, setFile] = useState<File | null | undefined>(null)
   const [error, setError] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault()
@@ -21,15 +22,18 @@ const ImportExcel = ({ close, addCertificates }: ImportExcelProps): ReactElement
       return
     }
 
+    setIsLoading(true)
     const formData = new FormData()
     formData.append('excel-file', file)
     void certificatesService.importExcel(formData)
       .then(response => {
         addCertificates(response)
+        setIsLoading(false)
         close()
       })
       .catch(error => {
         const { message } = error.data
+        setIsLoading(false)
         setError(message)
         console.log(error)
       })
@@ -57,18 +61,25 @@ const ImportExcel = ({ close, addCertificates }: ImportExcelProps): ReactElement
 
   return (
     <Modal>
-      <h1 className='block uppercase text-center text-xl font-bold'>Import excel</h1>
-      <form onSubmit={handleSubmit}>
-        <div className='mt-5'>
-          <input onChange={onChange} type="file" accept='.xlsx,.xlsm,.xls,.xlt,.xlsb' />
-          <p className='m-0 mt-1 text-red lowercase'>{error}</p>
-        </div>
+      <div className='p-5'>
+        {isLoading && (
+          <div className='bg-gray-light absolute top-0 left-0 w-full h-full rounded-xl after:absolute after:w-10 after:h-10 after:top-0 after:right-0 after:left-0 after:bottom-0 after:m-auto after:border-8 after:border-t-white after:opacity-100 after:rounded-[50%] after:animate-spin'>
+          </div>
+        )}
+        <h1 className='block uppercase text-center text-xl font-bold'>Import excel</h1>
+        <form onSubmit={handleSubmit}>
+          <div className='mt-5'>
+            <input onChange={onChange} type="file" accept='.xlsx,.xlsm,.xls,.xlt,.xlsb' />
+            <p className='m-0 mt-1 text-red lowercase'>{error}</p>
+          </div>
 
-        <div className='mt-5 flex gap-3 justify-center'>
-          <button className='bg-black text-white px-5 py-1 rounded-lg text-lg' type='button' onClick={close}>Cancel</button>
-          <button className='bg-red text-white px-5 py-1 rounded-lg text-lg' type='submit'>Submit</button>
-        </div>
-      </form>
+          <div className='mt-5 flex gap-3 justify-center'>
+            <button className='bg-black text-white px-5 py-1 rounded-lg text-lg' type='button' onClick={close}>Cancel</button>
+            <button className={'relative bg-red text-white px-5 py-1 rounded-lg text-lg'} type='submit'>Submit</button>
+          </div>
+        </form>
+      </div>
+
     </Modal>
   )
 }
